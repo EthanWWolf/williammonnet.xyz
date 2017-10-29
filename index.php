b5d967bf42b91a1c0f306e0f2863502901cf985e
William Monnet <monnetwilliam@gmail.com>
2017-10-29 23:16:53 +0100

    #6 Finition page cover, modification partie projet

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     setlocale(LC_TIME, "fr_FR.utf8");
     date_default_timezone_set('Europe/Paris');
 ?>
-<html>
+    <html>
+
     <head>
         <!--[if IE]>
           <link href="Accueil/CSS/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
@@ -15,60 +16,58 @@
         <link href="Bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Personnal CSS -->
         <link href="Accueil/CSS/stylesheets/style.css" media="screen, projection" rel="stylesheet" type="text/css" />
-        <!-- Custom styles for this template -->
-        <!--<link href="Accueil/CSS/stylesheets/cover.css" rel="stylesheet">-->
     </head>
+
     <body>
-        <div class="site-wrapper">
-            <div class="site-wrapper-inner">
-                <div class="cover-container">
-                    <header class="masthead clearfix">
-                        <div class="inner">
-                            <h3 class="masthead-brand">William Monnet</h3>
-                            <nav class="nav nav-masthead">
-                                <a class="nav-link active" href="#">Home</a>
-                                <a class="nav-link" href="#">Features</a>
-                                <a class="nav-link" href="#">Contact</a>
-                            </nav>
-                        </div>
-                    </header>
-                    <main role="main" class="inner cover">
-                        <h1 class="cover-heading">Développeur Web sur Toulouse et sa région</h1>
-                        <p class="lead">Bienvenue, vous retrouverez ici la liste des projets que j'ai effectué lors de mes diverses missions et formations personnels.</p>
-                        <p class="lead">
-                          <a href="#project" class="btn btn-lg btn-secondary">En savoir plus</a>
-                        </p>
-                    </main>
-                    <footer class="mastfoot">
-                        <div class="inner">
-                            <p>Powered by Bootstrap 4</p>
-                        </div>
-                    </footer>
+        <nav class=" navbar navbar-expand-lg navbar-light fixed-top navbar-shrink">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">William Monnet</a>
+            <div>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             </div>
-          </div>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="cover row">
+            <a href="#project" aria-hidden="true">
+            </a>
         </div>
-        <div class="row">
-            <article id="project" class="container-fluid row wrapper text-center center-block col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <a href="http://williammonnet.xyz" class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <div id="project" class="row">
+            <h1>Développeur Web sur Toulouse et sa région</h1>
+            <p>Bienvenue, vous retrouverez ici la liste des projets que j'ai effectué lors de mes diverses missions et formations personnels.</p>
+            <article class="container-fluid row wrapper text-center center-block col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <a href="http://williammonnet.xyz" class="col-sm-6 col-lg-4">
+                    <img src="Accueil/Img/Logo%20Drupal%208.png" alt="Site sous Drupal 8" class="img-fluid">
                     <section>
-                        <img src="Accueil/Img/Logo%20Drupal%208.png" alt="Site sous Drupal 8"
-                        class="img-fluid">
                         <h3>Mon site sous Drupal 8</h3>
                         <p>En construction</p>
                     </section>
                 </a>
-                <a href="http://williammonnet.xyz" class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                <a href="http://williammonnet.xyz" class="col-sm-6 col-lg-4">
+                    <img src="Accueil/Img/Logo%20Symfony%203.png" alt="Site sous Syphony 3" class="img-fluid">
                     <section>
-                        <img src="Accueil/Img/Logo%20Symfony%203.png" alt="Site sous Syphony 3"
-                        class="img-fluid">
-                        <h3>Mon site sous Syphony 3</h3>
+                        <h3>Mon site sous Symphony 3</h3>
                         <p>En construction</p>
                     </section>
                 </a>
-                <a href="http://aebgymtoulouse.e-monsite.com/" class="mx-auto col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                <a href="http://aebgymtoulouse.e-monsite.com/" class="col-sm-6 col-lg-4">
+                    <img src="Accueil/Img/Site%20AEB%20Gym%20Toulouse.png" alt="Site Aeb Gym" class="img-fluid">
                     <section>
-                        <img src="Accueil/Img/Site%20AEB%20Gym%20Toulouse.png" alt="Site Aeb Gym"
-                        class="img-fluid">
                         <h3>Le site de L'AEB Gym Toulouse</h3>
                     </section>
                 </a>
@@ -77,13 +76,16 @@
                 <legend class="container-fluid center-block col-xs-12 col-sm-12 col-md-12 col-lg-12">&laquo; Le développeur est une machine qui transforme du café en code &raquo;</legend>
             </footer>
         </div>
-            <!-- Bootstrap core JavaScript
+        <!-- Bootstrap core JavaScript
             ================================================== -->
-            <!-- Placed at the end of the document so the pages load faster -->
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script>window.jQuery || document.write('<script src="Bootstrap/assets/js/vendor/jquery.min.js"><\/script>')</script>
-            <script src="Bootstrap/assets/js/vendor/popper.min.js"></script>
-            <script src="Bootstrap/dist/js/bootstrap.min.js"></script>
-            <script src="Accueil/JS/script.js" crossorigin="anonymous"></script>
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script>
+            window.jQuery || document.write('<script src="Bootstrap/assets/js/vendor/jquery.min.js"><\/script>')
+        </script>
+        <script src="Bootstrap/assets/js/vendor/popper.min.js"></script>
+        <script src="Bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="Accueil/JS/script.js" crossorigin="anonymous"></script>
     </body>
-</html> 
+
+    </html>
